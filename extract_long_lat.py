228f9b5d60ab6eea1dfce51adc72423f3ffba1f0
Manh Ha Nguyen <manh.ha.3218@gmail.com>
2023-12-08 17:32:24 +1030

    back up after adjusting get lat long for WA

--- extract_long_lat.py
+++ extract_long_lat.py
@@ -20,19 +20,19 @@ def get_lat_lng(address):
         return None, None
 
 # Read the CSV file --> Pandas DataFrame
-file_path = "unique_addresses.csv"  # Replace with your actual file path
+file_path = "D:/UOA WORK/Summer Research/Data_Journalism_ASRS/unique_addresses.csv" # Replace with your actual file path
 df = pd.read_csv(file_path)
 
 # Create empty columns for latitude and longitude
-df['Latitude'] = None
-df['Longitude'] = None
+df['lat'] = None
+df['long'] = None
 
 # Geocode each address and fill in the latitude and longitude columns
 for index, row in df.iterrows():
-    address = row['ADDRESS']  
+    address = row['address']  
     lat, lng = get_lat_lng(address)
-    df.at[index, 'Latitude'] = lat
-    df.at[index, 'Longitude'] = lng
+    df.at[index, 'lat'] = lat
+    df.at[index, 'long'] = lng
 
 # Save the updated DataFrame back to the CSV file
 df.to_csv("output_file.csv", index=False)  
